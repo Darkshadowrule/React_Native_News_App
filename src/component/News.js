d5bb07aa6738d2d497ff32c4952fa7286c21a870
darkshadowrule <prajjwaldxi3@gmail.com>
2019-05-28 22:19:44 +0530

    Added Indicator while loading news

--- src/component/News.js
+++ src/component/News.js
@@ -1,30 +1,31 @@
 import React, { Component } from 'react';
-import { FlatList} from 'react-native';
+import { FlatList,View,ActivityIndicator} from 'react-native';
 import {getUSANews} from '../fetchNews'
 import Article from './Article'
 class News extends Component {
     state = {
         articles: [],
+        loading:true
     };
     componentWillMount()
     {   
         if(this.props.country=='All' && this.props.category=='All')
         {
-            const url='https://newsapi.org/v2/top-headlines?language=en&pagesize=50&apiKey=Your_Api_key'
+            const url='https://newsapi.org/v2/top-headlines?language=en&pagesize=50&apiKey=49e02f0e80bc4fa5a498a275e7e9d99f'
             this.fetchNews(url)
         }
         else if(this.props.country=='All' && this.props.category!='All')
         {
-            const url=`https://newsapi.org/v2/top-headlines?language=en&category=${this.props.category}&pagesize=50&apiKey=Your_Api_key`
+            const url=`https://newsapi.org/v2/top-headlines?language=en&category=${this.props.category}&pagesize=50&apiKey=49e02f0e80bc4fa5a498a275e7e9d99f`
             this.fetchNews(url)       
         }
         else if(this.props.country!='All' && this.props.category=='All')
         {
-            const url=`https://newsapi.org/v2/top-headlines?language=en&country=${this.props.country}&pagesize=50&apiKey=Your_Api_key`
+            const url=`https://newsapi.org/v2/top-headlines?language=en&country=${this.props.country}&pagesize=50&apiKey=49e02f0e80bc4fa5a498a275e7e9d99f`
             this.fetchNews(url)   
         }
         else{
-            const url=`https://newsapi.org/v2/top-headlines?language=en&country=${this.props.country}&category=${this.props.category}&pagesize=50&apiKey=Your_Api_key`
+            const url=`https://newsapi.org/v2/top-headlines?language=en&country=${this.props.country}&category=${this.props.category}&pagesize=50&apiKey=49e02f0e80bc4fa5a498a275e7e9d99f`
             this.fetchNews(url)
         }
         
@@ -32,7 +33,7 @@ class News extends Component {
     fetchNews = (url) => {
         getUSANews(url)
             .then(articles => {
-                this.setState({ articles});
+                this.setState({ articles,loading:false});
             })
             .catch((e) => {
         console.log(e)
@@ -40,6 +41,12 @@ class News extends Component {
     };
     render() {
         return (
+            this.state.loading
+            ?
+            <View style={{flex:1,justifyContent: 'center',alignItems: 'center',}}>
+                <ActivityIndicator size="large" color="#330066" animating/>
+            </View>
+            :
             <FlatList
                 data={this.state.articles}
                 renderItem={({ item }) => <Article article={item} />}
